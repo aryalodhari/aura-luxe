1cd8e221b119fc2cd72492d868b66fb6d6381dfc
Arya Lodhari <aryalodhari224@gmail.com>
2026-06-17 16:14:37 +0530

    Update routes

--- app/routes.py
+++ app/routes.py
@@ -45,3 +45,70 @@ def about():
 
 
 # ----------------------------- Authentication Routes -----------------------------------
+@bp.route('/register', methods=['GET','POST'])
+def register():
+    
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+
+        #validation
+        if not username or not email or not password:
+            return render_template('register.html', error='All Fields are required')
+
+        if password != confirm_password:
+            return render_template('register.html', error='Password do not match')
+        
+        #check if user exists
+        if User.query.filter_by(username=username).first():
+            return render_template('register.html',error='User already exists')
+        
+        if User.query.filter_by(email=email).first():
+            return render_template('register.html', error='Email already exists')
+        
+        #create new user 
+        hashed_password = generate_password_hash(password)
+        new_user = User(
+            username=username,
+            email=email,
+            password=hashed_password
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        #create cart for new user
+        new_cart = Cart(user_id=new_user.id)
+        db.session.add(new_cart)
+        db.session.commit()
+
+        return redirect (url_for('main.login'))
+    return render_template('register.html',page_title='Register - Aura By Honeyy')
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        #find user
+        user = User.query.fitler_by(username=username).first()
+
+        #verify password
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            session['username'] = user.username
+            session['is_admin'] = user.is_admin
+
+            return redirect(url_for('main.shop'))
+        else:
+            return render_template('login.html',error='Invaliud Username and Password')
+        
+    return render_template('login.html', page_title='Login - Aura By Honeyy')
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('main.index'))
